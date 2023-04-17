bb604c2e73941557eb234e0fba89dee25587e9c1
Sayan-2506 <urustemovsayan25@gmail.com>
2023-04-17 15:04:36 +0600

    update

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-export const API_URL = 'http://aia.pythonanywhere.com/api/v1/'
+export const API_URL = 'https://aia.pythonanywhere.com/api/v1/'
 
 const $api = axios.create({
     withCredentials: true,
