4be26bc140d883a69d525f37f5422f623de656d3
Sayan-2506 <urustemovsayan25@gmail.com>
2023-04-17 15:49:00 +0600

    update

--- src/components/API/server.jsx
+++ src/components/API/server.jsx
@@ -1,5 +1,5 @@
 const request_marh = {
-    url: 'https://translate.pythonanywhere.com/',
+    url: 'https://aia.pythonanywhere.com/',
     //url:'http://localhost:8000/',
     getLanguage:'api/v1/language/get/all/',
     getLanguageOut:'api/v1/language/out/get/all/'
